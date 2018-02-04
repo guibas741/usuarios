91ff17be3c663252d89e2e0a1a64ea03d53a0b1f
Guibas <bublitz.stein@gmail.com>
2018-02-04 17:27:12 -0200

    Adiciona página de menu e função de deslogar

--- autenticador.php
+++ autenticador.php
@@ -3,14 +3,17 @@
   $entrar = $_POST['entrar'];
   $senha = ($_POST['senha']);
   $connect = mysqli_connect('localhost','root','', 'db_php');
+ // session_start();
     if (isset($entrar)) {
       $verifica = mysqli_query($connect, "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'") or die("erro ao selecionar");
         if (mysqli_num_rows($verifica)<=0){
-          echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.php';</script>";
-          die();
+            echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.php';</script>";
+            die();
         }else{
-          setcookie("login",$login);
-          header("Location:listaUsuarios.php");
+            setcookie("login",$login);
+            //session_register("myusername");
+            //$_SESSION['login_user'] = $myusername;
+            header("Location:menu.php");
         }
     }
 ?>
---
+++ menu.php
@@ -0,0 +1,8 @@
+<?php
+  $login_cookie = $_COOKIE['login'];
+    if(isset($login_cookie)){
+      echo"Bem-Vindo, $login_cookie <br>";
+      echo"Essas informações <font color='red'>PODEM</font> ser acessadas por você";
+      echo"<br><a href='logout.php'>Logout</a>";
+    }
+?>
