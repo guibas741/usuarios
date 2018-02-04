a921241cfff3c2b6ed9a43bcd6eb231347e91677
Guibas <bublitz.stein@gmail.com>
2018-02-04 19:41:19 -0200

    Adiciona estilo ao formulario de login

--- login.php
+++ login.php
@@ -5,17 +5,21 @@
         <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     </head>
     <body>
-        <form method="post" action="autenticador.php" name="loginForm">
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="text" class="form-control" id="login" name="login" size="35" aria-describedby="emailHelp" placeholder="Enter login">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your data with anyone else.</small>
+        <div class="container">
+            <div class="row">
+                <form method="post" action="autenticador.php" name="loginForm">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="text" class="form-control" id="login" name="login" size="35" aria-describedby="emailHelp" placeholder="Enter login">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your data with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="senha" name="senha" size="35" placeholder="Senha">
+                    </div>
+                    <button type="submit" name="entrar" id="entrar" class="btn btn-primary">Entrar</button>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="senha" name="senha" size="35" placeholder="Senha">
-            </div>
-            <button type="submit" name="entrar" id="entrar" class="btn btn-primary">Entrar</button>
-        </form>
+        </div>
     </body>
 </html>
