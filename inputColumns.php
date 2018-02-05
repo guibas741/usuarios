acb27439f06fcb13a6598065b058d9181aa9a0cc
Guibas <bublitz.stein@gmail.com>
2018-02-04 23:34:25 -0200

    Adiciona arquivo de busca por valores das colunas

---
+++ inputColumns.php
@@ -0,0 +1,8 @@
+<?php
+    $id = filter_input(INPUT_GET, "id");
+    $nome = filter_input(INPUT_GET, "nome");
+    $sobrenome = filter_input(INPUT_GET, "sobrenome");
+    $email = filter_input(INPUT_GET, "email");
+    $login = filter_input(INPUT_GET, "login");
+    $senha = filter_input(INPUT_GET, "senha");
+?>
