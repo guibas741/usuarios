430d0e32448456c1a667ca89f6cd37bab5485040
Guibas <bublitz.stein@gmail.com>
2018-02-04 23:38:17 -0200

    Refatora função de edição de registro

--- editar.php
+++ editar.php
@@ -1,18 +1,16 @@
 <?php
-    $id = filter_input(INPUT_GET, "id");
-    $nome = filter_input(INPUT_GET, "nome");
-    $sobrenome = filter_input(INPUT_GET, "sobrenome");
-    $email = filter_input(INPUT_GET, "email");
-    $login = filter_input(INPUT_GET, "login");
-    $senha = filter_input(INPUT_GET, "senha");
+    include 'configDb.php';
+    include 'inputColumns.php';
 
-    $link = mysqli_connect("localhost", "root", "", "db_php");
-
-    if($link) {
-        $query = mysqli_query($link, "update usuarios set nome = '$nome',
-        sobrenome = '$sobrenome', email = '$email', login = '$login', 
-        senha = '$senha' 
-        where id = '$id';");
+    if($connection) {
+        $query = mysqli_query($connection, 
+        "update usuarios 
+            set nome  = '$nome',
+            sobrenome = '$sobrenome', 
+            email     = '$email', 
+            login     = '$login', 
+            senha     = '$senha' 
+            where id = '$id';");
         if($query) {
             ?>
             <script> 
@@ -21,9 +19,9 @@
             </script>
            <?php
         } else {
-            die("ERRO: ".mysqli_error($link));
+            die("ERRO: ".mysqli_error($connection));
         }
     } else {
-        die("ERRO: ".mysqli_error($link));
+        die("ERRO: ".mysqli_error($connection));
     }
 ?>
