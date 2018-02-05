aabbbcf1b18cda3868df815774476a05a24cbf0d
Guibas <bublitz.stein@gmail.com>
2018-02-04 23:34:46 -0200

    Refatora função salvar

--- salvar.php
+++ salvar.php
@@ -1,14 +1,9 @@
 <?php
-    $nome = filter_input(INPUT_GET, "nome");
-    $sobrenome = filter_input(INPUT_GET, "sobrenome");
-    $email = filter_input(INPUT_GET, "email");
-    $login = filter_input(INPUT_GET, "login");
-    $senha = filter_input(INPUT_GET, "senha");
+    include 'inputColumns.php';
+    include 'configDb.php';
 
-    $link = mysqli_connect("localhost", "root", "", "db_php");
-
-    if($link) {
-        $query = mysqli_query($link, "insert into usuarios(nome, sobrenome, email, login, senha) values('$nome', '$sobrenome', '$email', '$login', '$senha');");
+    if($connection) {
+        $query = mysqli_query($connection, "insert into usuarios(nome, sobrenome, email, login, senha) values('$nome', '$sobrenome', '$email', '$login', '$senha');");
         if($query) {
             ?>
             <script> 
@@ -17,10 +12,10 @@
             </script>
            <?php
         } else {
-            die("ERRO: ".mysqli_error($link));
+            die("ERRO: ".mysqli_error($connection));
         }
     } else {
-        die("ERRO: ".mysqli_error($link));
+        die("ERRO: ".mysqli_error($connection));
     }
 
 ?>
