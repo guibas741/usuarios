57089367c05dac798e6e3edaac1652c4e354ef48
guibas741 <bublitz.stein@gmail.com>
2018-02-27 12:59:31 -0300

    Update login.php
    Corrige label dos campos login e senha

--- login.php
+++ login.php
@@ -12,11 +12,11 @@
             
             <form method="post" action="autenticador.php" name="loginForm">
                 <div class="form-group col-lg-5">
-                    <label for="exampleInputEmail1">Usuário</label>
+                    <label for="login">Usuário</label>
                     <input type="text" class="form-control" required  id="login" name="login" size="35" placeholder="Enter login">
                 </div>
                 <div class="form-group col-lg-5">
-                    <label for="exampleInputPassword1">Senha</label>
+                    <label for="password">Senha</label>
                     <input type="password" class="form-control" required id="senha" name="senha" size="35" placeholder="Password">
                 </div>
                 <div class="form-group col-lg-5">
